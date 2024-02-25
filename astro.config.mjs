b226598037b9ba101a821a79432878eb48eb0b27
Jacob Asper <jacobasper191@gmail.com>
2024-02-25 00:29:52 -0500

    formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jacobasper.com',
-  integrations: [sitemap(), icon()]
+	site: 'https://jacobasper.com',
+	integrations: [sitemap(), icon()],
 });
